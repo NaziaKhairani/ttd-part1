46377aef054265df46ed2fa0aa01938e463d9b58
Nazia Khairani <Nazia Khairani@192.168.1.9>
2014-12-30 22:28:06 +0500

    Chapter 04

--- src/com/sibisoft/ttd/training/Dollar.java
+++ src/com/sibisoft/ttd/training/Dollar.java
@@ -1,21 +1,23 @@
 package com.sibisoft.ttd.training;
 /*
  * By Nazia Khairani
- * Chapter # 03
+ * Chapter # 04
  * Dated 30th December 2014
  */
 public class Dollar {
 
-	int amount;
+	private int amount;
 	
 	Dollar(int amount){
 		this.amount= amount;
+	}
+	
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
 	}
 	
 	public boolean equals(Object object) {
 		Dollar dollar= (Dollar) object;
 		return amount == dollar.amount;
 	}
-	
-	
 }
